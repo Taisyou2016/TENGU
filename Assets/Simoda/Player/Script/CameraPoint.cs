253005e071ac2374c38be65c5839cab642b5edb5
Syuvi <durchlaufregal@gmail.com>
2016-04-28 10:07:03 +0900

    カメラを簡易カメラから変更（プレイヤーについていく動作）

---
+++ Assets/Simoda/Player/Script/CameraPoint.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraPoint : MonoBehaviour
+{
+    public GameObject target;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        transform.position = target.transform.position + target.transform.forward * -3.0f + target.transform.up * 3.0f;
+
+        //if (Input.GetKey(KeyCode.Q))
+        //{
+        //    transform.Rotate(0, -120 * Time.deltaTime, 0);
+        //}
+
+        //if (Input.GetKey(KeyCode.E))
+        //{
+        //    transform.Rotate(0, 120 * Time.deltaTime, 0);
+        //}
+    }
+}
