c1cf1e243229e587af153e5554f44dd5ff86045a
scar <nohhipurasu001@yahoo.co.jp>
2016-04-28 11:41:18 +0900

    敵のふっとび

--- Assets/Simoda/Player/WindHit.cs
+++ Assets/Simoda/Player/WindHit.cs
@@ -4,24 +4,20 @@ using System.Collections;
 public class WindHit : MonoBehaviour
 {
     public GameObject enemy;
-    private TestEnemy TEnemy;
 
-    private Vector3 hitVector = Vector3.zero;
+    public Vector3 hitVector = Vector3.zero;
 
     public void OnCollisionEnter(Collision collision)
     {
-        print("hit1");
         if (collision.gameObject.tag == "Enemy")
         {
-            print("hit2");
-            TEnemy.Hit(hitVector, 10);
+            //TEnemy.Hit(hitVector, 10);
             //collision.rigidbody.AddForce(hitVector, ForceMode.Impulse);
         }
     }
 
     void Start()
     {
-        TEnemy = enemy.GetComponent<TestEnemy>();
     }
 
     void Update()
@@ -32,5 +28,10 @@ public class WindHit : MonoBehaviour
     public void SetVector(Vector3 vector)
     {
         hitVector = vector;
+    }
+
+    public Vector3 a()
+    {
+        return hitVector;
     }
 }
