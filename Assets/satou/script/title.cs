2d01c6e8c8f6f49e506599458110ee7aa0574885
sato-nayuta <dark040rami@ezweb.ne.jp>
2016-05-13 10:04:20 +0900

    タイトルからゲームプレイのシーン切り替え

---
+++ Assets/satou/script/title.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class title : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(Screen.width - 120, Screen.height - 40, 100, 20), "スタートボタン"))
+        {
+            Application.LoadLevel("GamePlay");
+        }
+    }
+}
