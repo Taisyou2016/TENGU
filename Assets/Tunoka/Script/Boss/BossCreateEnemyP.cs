d1fe48b81fe45e7e98192c54b6e241c7d6ce9eb7
tunomizu <tumikihurai46@gmail.com>
2016-06-03 11:17:26 +0900

    チュートリアル調整

--- Assets/Tunoka/Script/Boss/BossCreateEnemyP.cs
+++ Assets/Tunoka/Script/Boss/BossCreateEnemyP.cs
@@ -4,12 +4,12 @@ using System.Collections;
 public class BossCreateEnemyP : MonoBehaviour {
 
     // Use this for initialization
-    public GameObject tornadoPrefab;
+    public GameObject ele;
     void Start () {
         int count = 0;
         foreach (Transform child in transform)
         {
-            Instantiate(tornadoPrefab, child.transform.position, transform.rotation);
+            Instantiate(ele, child.transform.position, transform.rotation);
             count++;
         }
         Invoke("DeleteObj", 3.5f);
